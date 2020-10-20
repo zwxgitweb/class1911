0c2d7ecf41d8c6de401e2390a76ceae3e02f90bd
15990097911 <a1412945224@qq.com>
2020-10-20 15:26:02 +0800

    1020

--- 10.20/app.js
+++ 10.20/app.js
@@ -1,20 +1,36 @@
-/*
- * @FilePath: /l1/app.js
- * @Author: Florence
- * @Date: 2020-10-19 13:47:10
- * @Last Modified By: Florence
- * @LastEditTime: 2020-10-19 14:30:21
- */
+// const http = require('http');
 
-// require
 
-// 引入http包
-const http = require('http');
 
-// 创建端口号为3000的服务
-let app = http.createServer((req, res) => {
-	// 向前端返回值
-	res.end('hello world!');
-})
+// http.createServer((req, res) => {
+//     res.end(JSON.stringify({name:'lisi'}))
+// }).listen(3001);
 
-app.listen(8080);
+
+// let opt = {
+//     port: 3001,
+//     host: 'localhost',
+//     mehod: 'get'
+// }
+
+// let req = http.request(opt, (req, res) => {
+//     req.on('data')
+// })
+// req.end()
+
+// const  x = 'x';
+// const  y = 'y';
+
+// console.log(x,y)
+// console.log('我的%s已经%d岁', '猫', 2)
+// console.clear()
+// console.log('%o',Number)
+
+// 回声“＃hello-world-” >> README.md 
+// git init 
+// git add README.md 
+// git commit -m“首次提交” 
+// git branch -M main 
+// git remote add origin git@github.com：15990097911 / hello-world-。 git
+//  git push -u origin主
+                
