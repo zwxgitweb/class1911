ed4a69d94115f35ae45cd6b84a606f29861544ba
lvmaotang <3426889538@qq.com>
2020-10-20 16:39:48 +0800

    urlquery

--- 1020/url.js
+++ 1020/url.js
@@ -1,8 +1,16 @@
 const http = require('http')
 const url = require('url')
+const querystring = require('querystring')
 
 
 http.createServer((req, res) => {
-    console.log(url.parse);
-    // res.end('11')
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+
+    let result = url.parse(req.url)
+    if (result.pathname == '/api/list') {
+        let obj = querystring.parse(result.query)
+        res.end(`用户名：${obj.user}密码:${obj.pad}`)
+    } else {
+        res.end('123')
+    }
 }).listen(8080)
