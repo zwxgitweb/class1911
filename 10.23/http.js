e78e78f7e0d867f1881a0f17b34ab0147d16b959
wsnbb-dkl <1954069762@example.com>
2020-10-26 08:38:29 +0800

    zuoye

---
+++ 10.23/http.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const date = require('./mock/date.json')
+
+
+
+let server = http.createServer((a, b) => {
+    let urlo = url.parse(a.url) //返回对象
+    console.log(urlo)
+    if (urlo.path === '/fava.ico') {
+        b.end()
+    } else if (urlo.pathname === '/') {
+        fs.createReadStream('./index.html').pipe(b)
+    } else if (urlo.pathname === '/api/list') {
+        b.end(JSON.stringify(date))
+        console.log(date)
+    }
+    
+})
+server.listen(3000, () => {
+    console.log('server is at 3000')
+})
+// createReadStream 
